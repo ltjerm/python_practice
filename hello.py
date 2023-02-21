3f2bdad2c377af5aa48f2d754d6464fc5304913c
ltjerm <ltjerm@yahoo.com>
2023-02-21 07:52:31 -0700

    chapter_one

---
+++ hello.py
@@ -0,0 +1,23 @@
+# print ("Hello, World!")
+# message = "Hello world"
+# print(message)
+
+#Print Persinal Message 
+
+# my_name = 'Eric'
+# message = f'{my_name} would you like to learn python programming '
+# print (message)
+
+#Name Cases 
+new_name = 'Jeremy Hernandez'
+print (new_name.lower())
+print(new_name.capitalize())
+print (new_name.title())
+print (new_name.upper())
+
+#Famous quote 
+
+author = "Winston Churchill"
+quote = " Never, never, never give up."
+famous_quote = f'{author} once said, {quote}'
+print(famous_quote)
